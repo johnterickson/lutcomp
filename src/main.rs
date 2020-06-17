451b0361c7cbabbc199129eeaba8d2a3094085fa
John Erickson <john.t.erickson@gmail.com>
2020-06-17 01:23:15 -0700

    loops and terminates

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,10 @@ pub struct AssemblyParser;
 //     CopyImmTo()
 // }
 
+
+
+
+
 fn ucode() {
     println!("v2.0 raw");
 
@@ -38,32 +42,30 @@ fn ucode() {
 
     let instructions = 16u8;
     for i in 0..instructions {
-        for acc in 0..=255u8 {
-            for in2 in 0..=255u8 {
-                print!("# {:01x}{:02x}{:02x} ", i, acc, in2);
+        for in2 in 0..=255u8 {
+            for acc in 0..=255u8 {
+                print!("# {:01x}{:02x}{:02x} ", i, in2, acc);
 
                 match i {
-                    0b0000..=0b0011 => {
-                        let dst = i;
-                        println!("COPY_[IMM,ADDR,PC,MEM]_TO_{}", outputs[dst as usize]);
-                        print!("{:02x}{:02x}",  dst,  in2);
+                    0 => {
+                        println!("COPY");
+                        print!("{:02x}{:02x}",  0,  in2);
                     },
-                    0b0100 => {
-                        if in2 <= 0x3 {
-                            let dst = in2 & 0x3;
-                            println!("COPY_ACC_TO_{}", outputs[dst as usize]);
-                            print!("{:02x}{:02x}",  dst,  acc);
-                        } else {
-                            println!("COPY_ACC_TO_ RESERVED");
-                            print!("80FF");
-                        }
-                    }
-                    0b0101 => {
+                    1 => {
+                        println!("COPY_ACC");
+                        print!("{:02x}{:02x}",  0,  acc);
+                    },
+                    2 => {
+                        let sum = (in2 as u16) + (acc as u16);
+                        let flags = ((sum >> 8) & 0x1) as u8;
                         println!("ADD");
-                        let sum = (acc as u16) + (in2 as u16);
-                        let carry = ((sum >> 8) & 0x1) as u8;
-                        let flags = (carry << 2) | 0; // ACC
-                        print!("{:02x}{:02x}", flags, (sum & 0xFF) as u8);
+                        print!("{:02x}{:02x}",  flags,  sum as u8);
+                    },
+                    3 => {
+                        let sum = (in2 as u16) + (if acc == 0 { 3 } else {  1 });
+                        let flags = ((sum >> 8) & 0x1) as u8;
+                        println!("ADD3_F_ACC_0_ELSE_1");
+                        print!("{:02x}{:02x}",  flags,  sum as u8);
                     },
                     _ => {
                         println!("UNUSED");
