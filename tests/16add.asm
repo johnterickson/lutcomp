26a0891bad499ac94f84a680a6314707a4c9fd8f
John Erickson <john.t.erickson@gmail.com>
2020-06-20 21:40:24 -0700

    echo works on lut!

--- tests/16add.asm
+++ tests/16add.asm
@@ -1,43 +1,46 @@
-# add dword 1081 @ 0 to dword 4080 @ 2 and store @ 4
+# add dword 1081 @ 2 to dword 4080 @ 4 and store @ 6
 
 #init
-copy imm addr 0
-copy imm mem 81
-copy imm addr 1
-copy imm mem 10
 copy imm addr 2
-copy imm mem 80
+copy imm mem 81
 copy imm addr 3
+copy imm mem 10
+copy imm addr 4
+copy imm mem 80
+copy imm addr 5
 copy imm mem 40
 
 # add lo byte
-copy imm addr 0
-copy mem acc
 copy imm addr 2
-add mem acc
+copy mem acc
 copy imm addr 4
+add mem acc
+copy imm addr 6
 copy acc mem
 
 # add hi byte
-copy imm addr 1
-copy mem acc
 copy imm addr 3
-add mem acc
+copy mem acc
 copy imm addr 5
+add mem acc
+copy imm addr 7
 copy acc mem
 
 # if (sum + (-a) < 0) { carry = 1 }
 # negate a
-copy imm addr 0
+copy imm addr 2
 copy mem acc
 xor imm acc ff
 add imm acc 1
 # add sum
-copy imm addr 4
+copy imm addr 6
 add mem acc
-rotate imm acc f9
+# move the sign bit to bit0 - it is the sign bit
+rotleft imm acc -7
+# isolate it
 and imm acc 01
-copy imm addr 05
+
+copy imm addr 07
 add mem acc
 copy acc mem
-copy imm pc ff
+copy imm pc -1
