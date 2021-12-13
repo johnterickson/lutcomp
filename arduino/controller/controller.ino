ffc4d231eb7dd1504f4f44f3f29ff0361fcfd7a2
John Erickson <jerick@microsoft.com>
2021-12-12 20:27:06 -0800

    faster perf

--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -1,7 +1,6 @@
 
 #include <avr/interrupt.h>
-
-const int EMPTY = 0;
+#include <cppQueue.h>
 
 #if defined(AVR_UNO)
 
@@ -71,15 +70,16 @@ WiFiClient client;
 #endif
 
 const int QUEUE_LENGTH = 64;
-int inputQueue[QUEUE_LENGTH] = {0};
-int outputQueue[QUEUE_LENGTH] = {0};
+cppQueue  inputQueue(sizeof(char), QUEUE_LENGTH, FIFO, false);
+cppQueue  outputQueue(sizeof(char), QUEUE_LENGTH, FIFO, false);
 
 unsigned long tickCount = 0;
 //unsigned long breakPoint = 0;//1700/4*4;
 //long dataBreakPoint = 0x23;
 bool paused = false;
 bool halted = false;
-unsigned int delay_us = 0;
+#define DELAY_US  2
+#define INNER_TICKS (4*8)
 
 void initOutput(int pin, int initValue) {
   if (initValue != 0) {
@@ -99,36 +99,17 @@ void setDataPinsInput() {
 
 void setDataPinsOutput() {
   for(int i=0; i<8;i++) {
-    byte b = outputQueue[0] == EMPTY
-      ? 0
-      : ((outputQueue[0] & 0xFF) | 0x80);
+    byte b;
+    if(outputQueue.peek(&b)) {
+      b |= 0x80;
+    } else {
+      b = 0;
+    }
     initOutput(DATA_PINS[i], (b >> i) & 0x1);
   }
 }
 
-void init_queue(int queue[QUEUE_LENGTH]) {
-  for(int i=0; i < QUEUE_LENGTH; i++) queue[i] = EMPTY;
-}
 
-void enqueue(int queue[QUEUE_LENGTH], byte b) {
-  for(int i=0; i < QUEUE_LENGTH; i++) {
-    if (queue[i] == EMPTY) {
-      queue[i] = b;
-      break;
-    }
-  }
-}
-
-int dequeue(int queue[QUEUE_LENGTH]) {
-  int poppedValue = queue[0];
-  if (poppedValue != EMPTY) {
-    for(int i=0; i<QUEUE_LENGTH-1; i++) {
-      queue[i] = queue[i+1];
-    }
-    queue[QUEUE_LENGTH-1] = EMPTY;
-  }
-  return poppedValue;
-}
 
 byte readDataPins() {
   byte b = 0;
@@ -139,30 +120,19 @@ byte readDataPins() {
 }
 
 ISR(PCINT_vect){   // Port D, PCINT16 - PCINT23
-  static bool prevTTYIN_OE_ = false;
   bool newTTYIN_OE_ = digitalRead(TTYIN_OE_);
   if (newTTYIN_OE_ == HIGH) {
     setDataPinsInput();
   } else {
     setDataPinsOutput();
   }
-  if (!prevTTYIN_OE_ && newTTYIN_OE_) {
-    //Serial.println("TTYIN_OE_ LOW->HIGH");
-  } else if (prevTTYIN_OE_ && !newTTYIN_OE_) {
-    //Serial.print("TTYIN_OE_ HIGH->LOW: 0x");
-    //paused = true;
-    //byte b = readDataPins();
-    //Serial.print(b, HEX);
-    //Serial.println();
-  }
-  prevTTYIN_OE_ = newTTYIN_OE_;
-
+ 
   static bool prevTTYOUT_CP = false;
   bool newTTYOUT_CP = digitalRead(TTYOUT_CP);
   if (!prevTTYOUT_CP && newTTYOUT_CP) {
     byte b = readDataPins();
     if (b != 0) {
-      enqueue(inputQueue, b);
+      inputQueue.push(&b);
     }
   }
   prevTTYOUT_CP = newTTYOUT_CP;
@@ -170,13 +140,18 @@ ISR(PCINT_vect){   // Port D, PCINT16 - PCINT23
   static bool prevTTYIN_CP = false;
   bool newTTYIN_CP = digitalRead(TTYIN_CP);
   if (!prevTTYIN_CP && newTTYIN_CP) {
-    byte dequeued = dequeue(outputQueue) & 0x7f;
-    byte acked = readDataPins() & 0x7f;
-    if (dequeued != acked) {
-      Serial.print("TTL->ARDUINO ACK: 0x");
-      Serial.print(dequeued, HEX);
-      Serial.print(" vs 0x");
-      Serial.println(acked, HEX);
+    byte dequeued;
+    if (!outputQueue.pop(&dequeued)) {
+      //Serial.print("TTL->ARDUINO ACK: Nothing to dequeue.");
+    } else {
+      dequeued &= 0x7f;
+      byte acked = readDataPins() & 0x7f;
+      if (dequeued != acked) {
+        Serial.print("TTL->ARDUINO ACK: 0x");
+        Serial.print(dequeued, HEX);
+        Serial.print(" vs 0x");
+        Serial.println(acked, HEX);
+      }
     }
   }
   prevTTYIN_CP = newTTYIN_CP;
@@ -185,39 +160,12 @@ ISR(PCINT_vect){   // Port D, PCINT16 - PCINT23
 
 void tick(bool force) { 
   if (force || !paused) {
-    //digitalWrite(CLK, 1 ^ digitalRead(CLK));
-    CLK_PORT ^= (1<<CLK_BIT);
-    tickCount += 1;
-
-/*
-    if ((tickCount % 8) == 7){
-      char str[20];
-      byte b = readDataPins();
-      sprintf(str,"data_bus: %02x", b);
-      Serial.println(str);
-      Serial.flush();
-      if (b == dataBreakPoint){
-        Serial.print("DATA_BREAKPOINT: 0x");
-        Serial.println(b, HEX);
-        paused = true;
-      }
+    for(int i=0; i < INNER_TICKS; i++) {
+      CLK_PORT ^= (1<<CLK_BIT);
+      delayMicroseconds(DELAY_US);
+      tickCount += 1;
     }
-
-    unsigned long now = millis();
-    if ((now - lastUpdate) > 1000) {
-      //Serial.println(tickCount);
-      lastUpdate = now;
-    }
-    
-
-    if (tickCount == breakPoint) {
-      paused = true;
-      Serial.println("BREAKPOINT");
-    }
-    */
   }
-  //if (delay_us != 0) { delayMicroseconds(delay_us); }
-  //if (delay_ms != 0) { delay(delay_ms); }
 }
 
 void checkSerial() {
@@ -244,23 +192,39 @@ void checkSerial() {
     } else if (b == 'p') {
       paused = !paused;
     } else {
+      /*
+      Serial.print("#PC->Arduino @");
+      Serial.print(millis());
+      Serial.print(": ");
+      Serial.println(b, HEX);
+      */
       cli();
-      enqueue(outputQueue, b);
+      outputQueue.push(&b);
       sei();
     }
   }
 
- #if defined(ENABLE_WIFI)
+#if defined(ENABLE_WIFI)
   WiFiClient client = server.available();
 #endif
 
   cli();
-  if ((b = dequeue(inputQueue)) != EMPTY) {
+  if (inputQueue.pop(&b)) {
     sei();
-    //Serial.print("tty_out:");
-    //Serial.println((char)byteToWrite);
+
+    /* 
+    static char last_char = 0;
+  
+    if (last_char == '\n') {
+      Serial.print("#Arduino->PC @");
+      Serial.print(millis());
+      Serial.print(": ");
+      Serial.println(b, HEX);
+    }
+    */
     if (b > 0) {
       Serial.print((char)b);
+      //last_char = (char)b;
     }
     cli();
   }
@@ -279,8 +243,8 @@ void reset() {
   tick(true);
   tick(true);
   delay(100);
-  init_queue(inputQueue);
-  init_queue(outputQueue);
+  inputQueue.clean();
+  outputQueue.clean();
   tickCount = 0;
   halted = false;
   digitalWrite(RESET_, HIGH);
@@ -288,20 +252,22 @@ void reset() {
 }
 
 void loop() {
-  if ((tickCount & 0xFF) == 0) {
-    checkSerial();
-  }
-  
-  if(digitalRead(HALT) == HIGH) {
-      tick(false);
-  } else if (!halted) {
-      unsigned long start = millis();
-      do {
-        checkSerial();
-      } while ((millis() - start) < 1000);
-      Serial.print("HALT@");
-      Serial.println(tickCount);
-      halted = true;
+  for (int i=0; i< 4; i++) {
+    if ((tickCount & 0xFF) == 0) {
+      checkSerial();
+    }
+    
+    if(digitalRead(HALT) == HIGH) {
+        tick(false);
+    } else if (!halted) {
+        unsigned long start = millis();
+        do {
+          checkSerial();
+        } while ((millis() - start) < 1000);
+        Serial.print("HALT@");
+        Serial.println(tickCount);
+        halted = true;
+    }
   }
 }
 
