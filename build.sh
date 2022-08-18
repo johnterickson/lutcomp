02858d547ab010f940914c467591391a5df9c1ed
John T Erickson <johnterickson@github.com>
2022-08-18 16:41:49 -0700

    tested in HW

--- build.sh
+++ build.sh
@@ -17,13 +17,19 @@ cat logisim/test.txt | java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar 
 
 echo "6 7 * q" | cargo run --release -- compile programs/rpn.j --sim=true > logisim/rpn.hex
 
-# RAM stuff
+echo "Upload echo to RAM and run it"
 cargo run --release -- compile programs/hello_ram.j --image_base_address=080400 > logisim/hello_ram.hex
-(cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex | cargo run --release -- compile programs/bootram.j --sim=true) > logisim/bootram.hex
+cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex | cargo run --release -- compile programs/bootram.j --sim=true --block_for_stdin=true > logisim/bootram.hex
 
-# in circuit RAM
-cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/bootram.hex
+echo "Upload RPN to RAM and run it"
+cargo run --release -- compile programs/rpn.j --image_base_address=080400 > logisim/rpn_ram.hex
+(sleep 10 && cargo run --release -- program_ram --hex_path=./logisim/rpn_ram.hex && echo "6 7 * q") | cargo run --release -- compile programs/bootram.j --sim=true --block_for_stdin=true > logisim/bootram.hex
 
-#slow stuff
+# echo "Upload echo to logisim RAM"
+# cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/bootram.hex
+# echo "and run it"
+# (sleep 10 && cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex) | java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt
+
+echo "run RPN in logisim ROM"
 cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/rpn.hex
 echo "6 7 * q" | java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt
