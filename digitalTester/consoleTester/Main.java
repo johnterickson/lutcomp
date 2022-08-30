d36ec7035b6f3a511c9839f733fa191ec54a490b
John T Erickson <johnterickson@github.com>
2022-08-30 09:27:52 -0700

    fighting with eclipse omg

---
+++ digitalTester/consoleTester/Main.java
@@ -0,0 +1,26 @@
+package consoleTester;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.neemann.digital.testing.UnitTester;
+import de.neemann.digital.testing.UnitTester.TestException;
+import de.neemann.digital.core.NodeException;
+import de.neemann.digital.draw.elements.PinException;
+import de.neemann.digital.draw.library.ElementNotFoundException;
+import de.neemann.digital.gui.components.terminal.Keyboard;
+
+
+public class Main {
+		
+	public static void main(String[] args) throws TestException, IOException, ElementNotFoundException, PinException, NodeException {
+		var stdin = new StdInKeyboard();
+		
+		var tester = new UnitTester(new File(args[1]));
+		var keyboardNode = (Keyboard)tester.getNode(n -> n instanceof Keyboard);
+		keyboardNode.setKeyboard(stdin);
+		
+		tester.runToBreak();
+	}
+
+}
