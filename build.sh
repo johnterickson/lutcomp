25aa49053001d89e0617faa44bf4b64d79238e2d
John Erickson <john.t.erickson@gmail.com>
2022-09-03 20:31:03 -0700

    update build script to no longer use logisim

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@ set -e
 cargo test --release
 
 cargo run --release -- alu > logisim/alu.hex
-cargo run --release -- modify_circ --circuit=main --label=ALU_LUT --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/alu.hex
-
 cargo run --release -- ucode > logisim/ucode.hex
-cargo run --release -- modify_circ --circuit=main --label=uROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/ucode.hex
+
+echo Run Digital tests
+find logisim/*.dig
+find logisim/*.dig -exec java -cp ../Digital/target/Digital.jar CLI test -verbose -circ {} \;
 
 cargo run --release -- assemble programs/echo.asm > logisim/echo.asm.hex
-cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/echo.asm.hex
-java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt < logisim/test.txt
+cp logisim/echo.asm.hex logisim/rom.hex
+java -classpath ./digitalTester/consoleTester/bin:../Digital/target/Digital.jar consoleTester.ConsoleTester ./logisim/lutcomp.dig < logisim/test.txt
 
 cargo run --release -- compile programs/echo.j > logisim/echo.j.hex
-cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/echo.j.hex
-java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt < logisim/test.txt
+cp logisim/echo.j.hex logisim/rom.hex
+java -classpath ./digitalTester/consoleTester/bin:../Digital/target/Digital.jar consoleTester.ConsoleTester ./logisim/lutcomp.dig < logisim/test.txt
 
 echo "6 7 * q" | cargo run --release -- compile programs/rpn.j --sim=true > logisim/rpn.hex
 
 cp logisim/rpn.hex logisim/rom.hex
-find logisim/*.dig -exec java -cp ../Digital/target/Digital.jar CLI test -verbose -circ {} \;
 echo "6 7 * q" | java -classpath ./digitalTester/consoleTester/bin:../Digital/target/Digital.jar consoleTester.ConsoleTester ./logisim/lutcomp.dig
 
 echo "Upload echo to RAM and run it"
@@ -27,13 +27,12 @@ cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex | cargo ru
 
 echo "Upload RPN to RAM and run it"
 cargo run --release -- compile programs/rpn.j --image_base_address=080400 > logisim/rpn_ram.hex
-(sleep 10 && cargo run --release -- program_ram --hex_path=./logisim/rpn_ram.hex && echo "6 7 * q") | cargo run --release -- compile programs/bootram.j --sim=true --block_for_stdin=true > logisim/bootram.hex
+(cargo run --release -- program_ram --hex_path=./logisim/rpn_ram.hex && echo "6 7 * q") | cargo run --release -- compile programs/bootram.j --sim=true --block_for_stdin=true > logisim/bootram.hex
 
-# echo "Upload echo to logisim RAM"
-# cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/bootram.hex
-# echo "and run it"
-# (sleep 10 && cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex) | java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt
+echo "Upload echo to Digital RAM and run it"
+cp logisim/bootram.hex logisim/rom.hex
+(cargo run --release -- program_ram --hex_path=./logisim/hello_ram.hex) | java -classpath ./digitalTester/consoleTester/bin:../Digital/target/Digital.jar consoleTester.ConsoleTester ./logisim/lutcomp.dig
 
-echo "run RPN in logisim ROM"
-cargo run --release -- modify_circ --circuit=main --label=ROM --circ_path=./logisim/lutcomp.circ --hex_path=./logisim/rpn.hex
-echo "6 7 * q" | java -jar ./logisim/Logisim-Ita/Compiled/Logisim-ITA.jar logisim/lutcomp.circ -tty tty,speed,halt
+echo "run RPN in Digital ROM"
+cp logisim/rpn.hex logisim/rom.hex
+echo "6 7 * q" | java -classpath ./digitalTester/consoleTester/bin:../Digital/target/Digital.jar consoleTester.ConsoleTester ./logisim/lutcomp.dig
